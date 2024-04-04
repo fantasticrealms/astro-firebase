62804432a56061d2811a7cb27b4bdde508f1a459
Tony Pujals <subfuzion@users.noreply.github.com>
2024-04-03 21:23:18 -0700

    build: update client

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCN9zYWNlZKp1Cv4TfSmm0mBAUllk8YI3k',
-  authDomain: 'fantasticrealms-fbp.firebaseapp.com',
-  projectId: 'fantasticrealms-fbp',
-  storageBucket: 'fantasticrealms-fbp.appspot.com',
-  messagingSenderId: '211234519091',
-  appId: '1:211234519091:web:c1ee13c43dba99ab74cb46',
-  measurementId: "G-H7ME5E41GT",
+  apiKey: "AIzaSyCByg5M2h7dleV13j8A3VC2XpCgG_Ckrpo",
+  authDomain: "astro-firebase-ftw.firebaseapp.com",
+  projectId: "astro-firebase-ftw",
+  storageBucket: "astro-firebase-ftw.appspot.com",
+  messagingSenderId: "166766126682",
+  appId: "1:166766126682:web:dd188a0b6bb1df87f71a63",
+  measurementId: "G-F46JRB3NH3"
 };
 
 export const app = initializeApp(firebaseConfig);
